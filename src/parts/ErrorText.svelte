814cd7b16912821837c67ed6db0f79429d0e2aaf
Jordan <jordan@ghost.st>
2022-12-22 20:43:50 +1300

    Add ErrorText component for internal use

---
+++ src/parts/ErrorText.svelte
@@ -0,0 +1,25 @@
+
+<div class="ErrorText">
+  <p>
+    <slot />
+  </p>
+</div>
+
+
+<style lang="postcss">
+
+  .ErrorText {
+    display: block;
+    margin: 0;
+    padding: 0.3rem 0.6rem;
+    background: var(--error);
+    color: var(--node-text-color);
+    font-size: 0.9rem;
+    font-weight: bold;
+    border-radius: 4px;
+    line-height: 1.1rem;
+
+    p { margin: 0 }
+  }
+
+</style>
